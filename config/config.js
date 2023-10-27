afdf39593ae2ed680086683556f2a496796c7316
KodandRam <114702819+KodandRam@users.noreply.github.com>
2023-10-26 20:23:54 -0400

    Update config.js (#41)

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const config = {
-  
+  // Extract database configurations from environment variables
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
@@ -10,6 +10,18 @@ const config = {
     "dialect": process.env.DB_DIALECT,
     "port": parseInt(process.env.DB_PORT, 10)
   }
+}
+
+
+const useSSL = process.env.ENV_TYPE === "pulumi";
+
+if (useSSL) {
+  config.development.dialectOptions = {
+    "ssl": {
+      "require": true,
+      "rejectUnauthorized": false
+    }
+  };
 }
 
 module.exports = config;

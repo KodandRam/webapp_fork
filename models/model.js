f46fc9473c783839e9ea4cb488022f08b3e0fcde
KodandRam <114702819+KodandRam@users.noreply.github.com>
2023-11-29 15:33:47 -0500

    Models (#57)
    * changes to assingment.js
    
    * create new file
    
    * changes in model

--- models/model.js
+++ models/model.js
@@ -7,19 +7,25 @@ const config = require('../config/config');
 
 const Account = require('./Account');
 const Assignment = require('./Assignment');
-
+const Submission = require('./submission'); 
 
 const env = process.env.NODE_ENV || 'development';
 const currentConfig = config[env];
+console.log("current config", currentConfig);
 const db = new Sequelize({ ...currentConfig });
 
 // Initialize models
 // Sequelize database connection (instance)
 Account.initModel(db);
 Assignment.initModel(db);
+Submission.initModel(db); 
 
 // Define associations
 Account.hasMany(Assignment, { foreignKey: 'accountId', as: 'assignments' });
 Assignment.belongsTo(Account, { foreignKey: 'accountId', as: 'account' });
 
-module.exports = { db, Account, Assignment }
+// Assignment.hasMany(Submission, { foreignKey: 'assignment_id', as: 'submissions', onDelete: 'CASCADE' });
+Assignment.hasMany(Submission, { foreignKey: 'assignment_id', as: 'submissions' });
+Submission.belongsTo(Assignment, { foreignKey: 'assignment_id', as: 'assignment' });
+
+module.exports = { db, Account, Assignment, Submission}

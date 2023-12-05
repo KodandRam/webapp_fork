f94e57a2e08a79a1d98c702076b9407d742fd78a
KodandRam <114702819+KodandRam@users.noreply.github.com>
2023-12-05 02:23:24 -0500

    Update server.js (#85)

--- server.js
+++ server.js
@@ -1,30 +1,19 @@
 // Loading environment variables from .env file
 require('dotenv').config();
-
+const path = require('path');
 const app = require('./app');
-
-const processCsv = require('./helpers/userImporter');
-
+const { processCsv } = require('./helpers/userImporter');  // Assuming processCsv is exported from userImporter
 // Importing Sequelize database connection (instance)
 const { db } = require('./models/model');
-const processCsv = require('./helpers/userImporter');
-const logger = require('./log');
 
 const PORT = process.env.PORT || 8080;
 
-// Sync the database and start the server 
-// Set force to false to avoid dropping tables
-
-// alter: true: Adjusts database tables to match model definitions. 
-// Adds or removes columns as necessary without dropping tables. 
-// Useful for updating the database schema after changes to models
-
 let filePath = path.join(__dirname, '/opt/users.csv');
+console.log('ENV_TYPE: ', process.env.ENV_TYPE);
 
 if (process.env.ENV_TYPE === 'DEBIAN_VM') {
-    filePath = '/opt/users.csv'
-}
-else if (process.env.ENV_TYPE === 'GITHUB_CI') {
+    filePath = '/opt/users.csv';
+} else if (process.env.ENV_TYPE === 'GITHUB_CI') {
     filePath = path.join(__dirname, '/opt/users.csv');
 }
 
@@ -32,14 +21,10 @@ db.sync({ force: false, alter: true })
     .then(() => {
         processCsv(filePath);
         app.listen(PORT, () => {
-            logger.info(`Web server running on http://localhost:${PORT}`);
             console.log(`Web Server running on http://localhost:${PORT}`);
         });
     })
     .catch((error) => {
-        logger.error('Error syncing database:', error);
         console.error('Error syncing database:', error);
         process.exit(1);
     });
-
-

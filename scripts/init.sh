35e95866fe8559cdce8ae4a799846be81594caa2
KodandRam <114702819+KodandRam@users.noreply.github.com>
2023-12-05 01:52:12 -0500

    Update init.sh (#83)

--- scripts/init.sh
+++ scripts/init.sh
@@ -51,13 +51,16 @@ sudo chown -R webapp_user:webapp_user /home/webapp_user
 sudo chmod -R 755 /home/webapp_user
 
 # Create a log directory for webapp
+echo "Checking if /var/log/webapp exists..."
 if [ ! -d /var/log/webapp ]; then
+    echo "Directory does not exist. Creating..."
     sudo mkdir -p /var/log/webapp
+    sudo chmod 755 /var/log/webapp
     sudo chown -R webapp_user:webapp_user /var/log/webapp
+else
+    echo "Directory already exists."
 fi
 sudo chmod 755 /var/log/webapp
-sudo mkdir -p /var/log/webapp
-
 cd /home/webapp_user/webapp || { echo "Directory not found"; exit 1; }
 # cp /opt/users.csv /home/webapp_user/webapp/opt/users.csv
 
